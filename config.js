fe45ba758c77ffcfd7ed06bffd0844e47039e6ed
Per Lind <per.lind@xperta.se>
2018-03-07 19:43:42 +0100

    structure

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const config = {};
+const config = {}
 
-config.endpoint = process.env.DB_ENDPOINT;
-config.primaryKey = process.env.DB_PRIMARY_KEY;
-config.database = {"id": process.env.DB_ID };
-config.collection = { "id": process.env.DB_COLLECTION };
+config.host = process.env.DB_ENDPOINT || "[URI for DocumentDB endpoint]";
+config.authKey = process.env.DB_PRIMARY_KEY || "[Master key for DocumentDB]";
+config.databaseId = "iotDB";
+config.collectionId = "iotMessages";
 
 module.exports = config;
