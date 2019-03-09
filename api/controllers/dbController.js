bc6dec2200bbfd4f7558180dff705cb260b9bdfe
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2019-03-09 19:22:10 +0100

    Validate payload before saving iot data

--- api/controllers/dbController.js
+++ api/controllers/dbController.js
@@ -8,12 +8,27 @@ const data = db => (req, res) => {
 }
 
 const upload = db => (req, res) => {
-  db.collection('measures').insertOne(req.body);
-  res.status(200).json({
-    data: {
-      Response: []
-    }
-  });
+  const data = req.body;
+  console.log("Received new data: ", data)
+  // Validate presence of measures and timestamp
+  const { timestamp, measures } = data;
+  const errors = [];
+  if (typeof timestamp !== 'string' || timestamp === '') {
+    errors.push("timestamp must be given and have type string");
+  }
+  if (typeof measures !== 'object') {
+    errors.push("measures must be given and have type json");
+  }
+  if (errors.length > 0) {
+    console.log("Payload not valid: ", errors)
+    return res.status(400).json({ errors })
+  }
+  return db.collection('measures').insertOne(data)
+    .then(res.status(200).json({}))
+    .catch(error => {
+      console.log("Error when inserting document: ", error);
+      res.status(500).json({});
+    });
 }
 
 const dbController = db => ({
