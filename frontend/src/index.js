47e93e172db8bba5c2e17b259ed83c63bd1bf5af
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2019-03-26 19:20:27 +0100

    Add custom theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
-
+import theme from './styles/jss/theme';
 import './styles/css/main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Root() {
+  return (
+    <MuiThemeProvider theme={createMuiTheme(theme)}>
+      <App />
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ frontend/src/styles/jss/theme.js
@@ -0,0 +1,15 @@
+import teal from '@material-ui/core/colors/teal';
+
+const theme = {
+  palette: {
+    primary: teal,
+  },
+  shape: {
+    borderRadius: 0,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+};
+
+export default theme;
