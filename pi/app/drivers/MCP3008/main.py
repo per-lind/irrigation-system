0073a9785ff4004b9ed2573d574f2180284ea56e
Anu Lind <anu-maria@hotmail.com>
2019-06-16 15:42:04 +0200

    Read all pins on MCP3008

--- pi/app/drivers/MCP3008/main.py
+++ pi/app/drivers/MCP3008/main.py
@@ -1,5 +1,6 @@
 from utils.Driver import Driver
 import math
+import time
 
 class MCP3008(Driver):
   def __init__(self, config, cs, miso, mosi, clk):
@@ -30,9 +31,25 @@ class MCP3008(Driver):
     self.sensor = MCP.MCP3008(spi, self.cs)
 
   def _read(self, payload={}):
+    result = {}
+
+    for x in range(0, 8):
+      try:
+        for y in range(0, 3):
+          pin = getattr(self.MCP, "P{}".format(x))
+          value = self.sensor.read(pin)
+          print("Reading pin {}: {}".format(pin, value))
+          time.sleep(0.5)
+        print("")
+      except Exception as inst:
+        print('Failed to read pin {}'.format(x))
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+
     chan7 = self.sensor.read(self.MCP.P7)
     rV = ((1024.0/chan7) - 1.0)*10000.0
     value = round(((1/(1.125614740E-03 + (2.346500768E-04*math.log(rV)) + (0.8600178326E-07*math.pow(math.log(rV), 3))))-273.15),3)
-    return {
-      'output': value,
-    }
+    result['chan7'] = value
+
+    return result
