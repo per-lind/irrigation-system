af39345d88b016ea79eefd23419a729eeed21e92
Anu Lind <anu-maria@hotmail.com>
2019-07-04 21:37:51 +0200

    Simplify method invoke frontend

--- frontend/src/components/Hardware/Action.js
+++ frontend/src/components/Hardware/Action.js
@@ -5,46 +5,17 @@ import Button from '@material-ui/core/Button';
 import Input from '../Input';
 import { formatter } from '../../utilities';
 import Divider from '@material-ui/core/Divider';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
-import classnames from 'classnames';
 import CardContent from '@material-ui/core/CardContent';
-import { Done, Close, ExpandMore } from '@material-ui/icons';
+import { Done, Close } from '@material-ui/icons';
 
 const styles = theme => ({
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  card: {
-    maxWidth: 400,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  actions: {
-    display: 'flex',
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
+  content: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
   },
   go: {
-    display: "flex",
-    justifyContent: "flex-end",
+    marginTop: theme.spacing(1),
   },
   success: {
     color: theme.palette.primary.dark,
@@ -52,18 +23,16 @@ const styles = theme => ({
   failure: {
     color: theme.palette.error.dark,
   },
+  data: {
+    marginTop: theme.spacing(2),
+  },
 });
 
 class Action extends Component {
   state = {
     selected: {},
-    expanded: false,
     success: undefined,
     failure: undefined,
-  };
-
-  handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
   };
 
   handleChange = (name, value) => {
@@ -71,7 +40,7 @@ class Action extends Component {
   };
 
   handleClick = () => {
-    this.setState({ expanded: true, success: undefined, failure: undefined });
+    this.setState({ success: undefined, failure: undefined });
     this.props.onClick(this.state.selected)
       .then(() => this.setState({ success: true }))
       .catch(() => this.setState({ failure: true }))
@@ -94,7 +63,6 @@ class Action extends Component {
     } = this.props;
 
     const {
-      expanded,
       success,
       failure,
     } = this.state;
@@ -102,72 +70,46 @@ class Action extends Component {
     return (
       <React.Fragment>
         <Divider />
-        <CardActions className={classes.actions}>
-          <Button
-            size="small"
-            color="primary"
-            onClick={payload ? this.handleExpandClick : this.handleClick}
-            disabled={!payload && loading}
-          >
-            {payload ? "OPEN" : "READ"}
-          </Button>
-          {(value || payload) &&
-            <IconButton
-              className={classnames(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={this.handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="Show more"
-            >
-              <ExpandMore />
-            </IconButton>
+        <CardContent classes={{root: classes.content}}>
+          {payload &&
+            Object.keys(payload).map((payloadId, index) => {
+              const item = payload[payloadId];
+              return (
+                <Input
+                  key={index}
+                  {...item}
+                  label={item.name || payloadId}
+                  id={payloadId}
+                  onChange={this.handleChange}
+                  value={this.state.selected[payloadId]}
+                />
+              );
+            })
           }
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            {payload &&
-              Object.keys(payload).map((payloadId, index) => {
-                const item = payload[payloadId];
-                return (
-                  <Input
-                    key={index}
-                    {...item}
-                    label={item.name || payloadId}
-                    id={payloadId}
-                    onChange={this.handleChange}
-                    value={this.state.selected[payloadId]}
-                  />
-                );
-              })
-            }
-            {payload &&
-              <div className={classes.go}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                  onClick={this.handleClick}
-                  disabled={loading}
-                >
-                  GO
-                </Button>
-              </div>
-            }
-            {success && <Done className={classes.success} />}
-            {failure && <Close className={classes.failure} />}
-            {value &&
-              Object.keys(value).map(key => {
-                const { name, unit } = response[key] || {};
-                return (
-                  <Typography key={key} className={classes.data} >
-                    {name || key}: {formatter[unit] ? formatter[unit](value[key]) : `${value[key]} ${unit}`}
-                  </Typography>
-                )
-              })
-            }
-          </CardContent>
-        </Collapse>
+          <div className={classes.go}>
+            <Button
+              variant="contained"
+              size="small"
+              color="primary"
+              onClick={this.handleClick}
+              disabled={loading}
+            >
+              {payload ? "GO" : "READ"}
+            </Button>
+          </div>
+          {success && <Done className={classes.success} />}
+          {failure && <Close className={classes.failure} />}
+          {value &&
+            Object.keys(value).map(key => {
+              const { name, unit } = response[key] || {};
+              return (
+                <Typography key={key} className={classes.data} >
+                  {name || key}: {formatter[unit] ? formatter[unit](value[key]) : `${value[key]} ${unit}`}
+                </Typography>
+              )
+            })
+          }
+        </CardContent>
       </React.Fragment>
     )
   }
